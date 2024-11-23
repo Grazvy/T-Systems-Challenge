55e311fdfbb0387235eb5f7cee017d71c9acfa86
Grazvy <Grazvydas.Kuras@gmail.com>
2024-11-23 17:02:58 +0100

    decision variables

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ model.customer_destination_distance = pyo.Param(model.customers, initialize={"id
 model.next_customer_distance = pyo.Param(model.customers, model.customers, initialize={("id1", "id2"): 50}, doc="nc_d")
 model.vehicle_customer_distance = pyo.Param(model.vehicles, model.customers, initialize={("id1", "id2"): 50}, doc="vc_d")
 model.value_function = pyo.Param(model.customers, initialize={"id1": 5,"id2": 5}, doc="v")
+
+model.customer_customer = pyo.Var(model.customers, model.customers, within=pyo.Binary, doc="x_cc")
+model.vehicle_customer = pyo.Var(model.vehicles, model.customers, within=pyo.Binary, doc="x_vc")
+model.waiting_time = pyo.Var(model.customers, within=pyo.NonNegativeIntegers, doc="w")
